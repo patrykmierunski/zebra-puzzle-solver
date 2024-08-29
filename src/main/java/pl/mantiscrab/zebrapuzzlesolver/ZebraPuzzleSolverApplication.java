6f634f8495002a2fc234d6c697b042afadbd1b71
patrykmierunski <93212605+patrykmierunski@users.noreply.github.com>
2024-08-29 10:55:14 +0200

    Initial files

---
+++ src/main/java/pl/mantiscrab/zebrapuzzlesolver/ZebraPuzzleSolverApplication.java
@@ -0,0 +1,13 @@
+package pl.mantiscrab.zebrapuzzlesolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZebraPuzzleSolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZebraPuzzleSolverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mantiscrab/zebrapuzzlesolver/ZebraPuzzleSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mantiscrab.zebrapuzzlesolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZebraPuzzleSolverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
